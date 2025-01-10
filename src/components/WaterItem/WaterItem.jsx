efbe70d0aed80d52df241b7284fd34c4a0ad65de
Alexey <mirar2246@gmail.com>
2025-01-10 23:30:45 +0200

    alexey-info-daily-item-list

--- src/components/WaterItem/WaterItem.jsx
+++ src/components/WaterItem/WaterItem.jsx
@@ -1,12 +1,29 @@
 import s from "./WaterItem.module.css";
-
+import sprite from "../../assets/sprite.svg";
 
 const WaterItem = ({ volume, time }) => {
   return (
-    <div className={s.item}>
-      <p>{volume}ml</p>
-      <p>{time}</p>
-    </div>
+    <li className={s.item}>
+      <svg className={s.iconGlass} width="38" height="38">
+        <use href={`${sprite}#icon-mage_water-glass-fill`}></use>
+      </svg>
+      <div className={s.dataWrapper}>
+        <p className={s.textVolume}>{volume}ml</p>
+        <p className={s.textTime}>{time}</p>
+      </div>
+      <div className={s.dataWrapper}>
+        <button type="button">
+          <svg className={s.iconChange} width="14" height="14">
+            <use href={`${sprite}#icon-edit-2`}></use>
+          </svg>
+        </button>
+        <button type="button">
+          <svg className={s.iconDell} width="14" height="14">
+            <use href={`${sprite}#icon-trash-04`}></use>
+          </svg>
+        </button>
+      </div>
+    </li>
   );
 };
 export default WaterItem;
