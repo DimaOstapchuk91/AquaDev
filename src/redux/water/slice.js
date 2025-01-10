68a9a23356bcfc95f5a23ee9ce03d42c41a1c90b
Daria Havlovych <daria.havlovych@gmail.com>
2025-01-10 22:34:36 +0200

    water slice

---
+++ src/redux/water/slice.js
@@ -0,0 +1,28 @@
+import { createSlice, isAnyOf } from "@reduxjs/toolkit";
+import { fetchDailyWaterInfo } from "./operations.js";
+
+const initialState = {
+  waterInfo: {
+    // dailyNorma: 1500,
+    waterPortions: [],
+    totalWater: 0,
+    error: null,
+  },
+};
+
+const slice = createSlice({
+  name: "waterInfo",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchDailyWaterInfo.fulfilled, (state, action) => {
+        state.waterInfo.waterPortions = action.payload.waterPortions;
+        state.waterInfo.totalWater = action.payload.totalWater;
+      })
+      .addCase(fetchDailyWaterInfo.rejected, (state, action) => {
+        state.error = action.payload;
+      });
+  },
+});
+
+export const waterReducer = slice.reducer;
