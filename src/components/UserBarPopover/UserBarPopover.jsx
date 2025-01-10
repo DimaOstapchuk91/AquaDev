a217b62b2638f6fb7979413b52d3fa2eee14d1bf
Miroszlav <miroszlav.popovics@gmail.com>
2025-01-10 21:30:02 +0200

    Miroslav-UserBar

--- src/components/UserBarPopover/UserBarPopover.jsx
+++ src/components/UserBarPopover/UserBarPopover.jsx
@@ -1,31 +1,33 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { LuSettings } from "react-icons/lu";
 import { LuLogOut } from "react-icons/lu";
 
 import s from "./UserBarPopover.module.css";
 
-const UserBarPopover = ({ onClose, onSetting, onLogout }) => {
+const UserBarPopover = ({ buttonRef, onClose, openModal }) => {
   const popoverRef = useRef(null);
 
   useEffect(() => {
     const handleClickOutside = (event) => {
-      if (popoverRef.current && !popoverRef.current.contains(event.target)) {
+      if (
+        popoverRef.current &&
+        !popoverRef.current.contains(event.target) &&
+        !buttonRef.current.contains(event.target)
+      ) {
         onClose();
       }
     };
     document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [onClose]);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [onClose, buttonRef]);
 
   return (
     <div ref={popoverRef} className={s.popoverContainer}>
-      <button onClick={onSetting} className={s.popoverBtn}>
+      <button onClick={() => openModal("Setting")} className={s.popoverBtn}>
         <LuSettings className={s.popoverIcons} /> Setting
       </button>
-      <button onClick={onLogout} className={s.popoverBtn}>
+      <button onClick={() => openModal("Logout")} className={s.popoverBtn}>
         <LuLogOut className={s.popoverIcons} /> Logout
       </button>
     </div>
--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -1,25 +1,13 @@
 // import { useState } from "react";
 import UserBar from "../UserBar/UserBar";
-// import ReactDOM from "react-dom";
-
-// import { IoSettingsOutline } from "react-icons/io5";
-// import { LuLogOut } from "react-icons/lu";
 
 const UserPanel = ({ name, avatar }) => {
-  // const [settingModalOpen, setSettingModalOpen] = useState(false);
-  // const [logoutModalOpen, setLogoutModalOpen] = useState(false);
-
   return (
     <div>
       <h2>
         Hello, <span>{name}Nadia!</span>
       </h2>
-      <UserBar
-        name={name}
-        avatar={avatar}
-        // onSetting={() => setSettingModalOpen(true)}
-        // onLogout={() => setLogoutModalOpen(true)}
-      />
+      <UserBar name={name} avatar={avatar} />
     </div>
   );
 };
