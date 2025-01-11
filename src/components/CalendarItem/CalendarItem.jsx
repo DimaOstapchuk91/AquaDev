aaebc9a79384ff3c63bb369e661173ece35d037b
xd.faunredd <andrew.furevych@gmail.com>
2025-01-11 17:39:44 +0200

    add a start version of the chart

--- src/components/CalendarItem/CalendarItem.jsx
+++ src/components/CalendarItem/CalendarItem.jsx
@@ -1,18 +1,18 @@
 import s from "./CalendarItem.module.css";
 
-const CalendarItem = ({ day, isToday, isSelected, onClick }) => {
+const CalendarItem = ({ day, isToday, isSelected, onClick, percentage }) => {
   return (
-    <div>
+    <li>
       <button
         className={`${s.calendarItem} ${isToday ? s.currentDay : ""} ${
           isSelected ? s.selected : ""
-        }`}
+        } , ${percentage !== 100 ? s.notAchievedYet : ""}`}
         onClick={() => onClick(day)}
       >
         {day}
       </button>
-      <p className={s.percentage}>0%</p>
-    </div>
+      <p className={s.percentage}> {percentage}%</p>
+    </li>
   );
 };
 
