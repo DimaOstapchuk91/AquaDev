b8246ad15530f40818f2d348eb1f574b8273b469
Yarik2019 <yroslavterno@gmail.com>
2025-01-11 16:45:51 +0200

    add code media 1440

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,13 +1,30 @@
+import { useState, useEffect } from 'react';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import AdvantagesSection from '../../components/AdvantagesSection/AdvantagesSection';
 import styles from './SignUpPage.module.css';
 const SignUpPage = () => {
+  const [isVisible, setIsVisible] = useState(window.innerWidth > 1440);
+
+  useEffect(() => {
+     const handleResize = () => {
+      setIsVisible(window.innerWidth > 1440);
+    };
+
+    // Додаємо слухач події
+    window.addEventListener("resize", handleResize);
+
+    // Видаляємо слухач події під час розмонтування
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className={styles.containerWapper}>
       <SignUpForm />
-      <div className={styles.rightSection}>
+       {isVisible && <div className={styles.rightSection}>
         <AdvantagesSection />
-      </div>
+      </div>}
     </div>
   );
 };
