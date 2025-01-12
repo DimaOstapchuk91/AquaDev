d74c45ba37d1c881da84bd0eeb9162a9da2a0d08
Yarik2019 <yroslavterno@gmail.com>
2025-01-12 02:21:16 +0200

    add Yaroslav-signin-and-signup-styles

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,12 +1,32 @@
+import { useState, useEffect } from 'react';
 import SignInForm from '../../components/SignInForm/SignInForm';
 import AdvantagesSection from '../../components/AdvantagesSection/AdvantagesSection';
 import styles from './SignInPage.module.css';
 const SignInPage = () => {
+  const [isVisible, setIsVisible] = useState(window.innerWidth > 1440);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsVisible(window.innerWidth > 1440);
+    };
+
+    // Додаємо слухач події
+    window.addEventListener("resize", handleResize);
+
+    // Видаляємо слухач події під час розмонтування
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
-    <div className={styles.containerWapper}>
-      <SignInForm />
-      <div className={styles.rightSection}>
-        <AdvantagesSection />
+    <div className={"container"}>
+      <div className={styles.containerWapper}>
+        <SignInForm />
+        {isVisible && (
+         <div className={styles.rightSection}>
+          <AdvantagesSection />
+         </div>
+        )}
       </div>
     </div>
   );
