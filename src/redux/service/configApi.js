4415e746bab857a5e8f3f7522d649bb1ca88e02c
Daria Havlovych <daria.havlovych@gmail.com>
2025-01-12 22:28:29 +0200

    change baseUrl

--- src/redux/service/configApi.js
+++ src/redux/service/configApi.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const aquaDevApi = axios.create({
-  //baseURL: "https://aquadev-back.onrender.com",
-  baseURL: "http://localhost:3000",
+  baseURL: "https://aquadev-back.onrender.com",
+  //baseURL: "http://localhost:3000",
 
   headers: {
     "Content-Type": "application/json",
