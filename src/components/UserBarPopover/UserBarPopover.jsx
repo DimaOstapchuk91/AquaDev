d436c4a5b4647e3a650fafa3ebbfef79d3a46e39
Miroszlav <miroszlav.popovics@gmail.com>
2025-01-14 16:12:15 +0200

    add modal open setting and logout

--- src/components/UserBarPopover/UserBarPopover.jsx
+++ src/components/UserBarPopover/UserBarPopover.jsx
@@ -1,38 +1,57 @@
-import { useEffect, useRef } from "react";
+import { useRef, useState } from "react";
+
 import sprite from "../../assets/sprite.svg";
 import s from "./UserBarPopover.module.css";
 
-const UserBarPopover = ({ buttonRef, onClose, openModal }) => {
+import UserSettingsModal from "../Modal/UserSettingsModal/UserSettingsModal.jsx";
+import LogOutModal from "../Modal/LogOutModal/LogOutModal.jsx";
+
+import Modal from "../Modal/Modal.jsx";
+
+const UserBarPopover = () => {
+  const [settingModalOpen, setSettingModalOpen] = useState(false);
+  const [logoutOpen, setlogoutOpen] = useState(false);
   const popoverRef = useRef(null);
 
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (
-        popoverRef.current &&
-        !popoverRef.current.contains(event.target) &&
-        !buttonRef.current.contains(event.target)
-      ) {
-        onClose();
-      }
-    };
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => document.removeEventListener("mousedown", handleClickOutside);
-  }, [onClose, buttonRef]);
+  const handleSettingOpen = () => {
+    setSettingModalOpen(true);
+  };
+  const handleSettingClose = () => {
+    setSettingModalOpen(false);
+  };
+
+  const handleLogoutOpen = () => {
+    setlogoutOpen(true);
+  };
+  const handleLogoutClose = () => {
+    setlogoutOpen(false);
+  };
 
   return (
     <div ref={popoverRef} className={s.popoverContainer}>
-      <button onClick={() => openModal("Setting")} className={s.popoverBtn}>
-        <svg className={s.popoverIcons} width={16} height={16}>
-          <use href={`${sprite}#icon-settings`}></use>
-        </svg>
-        Setting
-      </button>
-      <button onClick={() => openModal("Logout")} className={s.popoverBtn}>
-        <svg className={s.popoverIcons} width={16} height={16}>
-          <use href={`${sprite}#icon-log-out`}></use>
-        </svg>
-        Logout
-      </button>
+      <div>
+        <button onClick={handleSettingOpen} className={s.popoverBtn}>
+          <svg className={s.popoverIcons} width={16} height={16}>
+            <use href={`${sprite}#icon-settings`}></use>
+          </svg>
+          Setting
+        </button>
+        <button onClick={handleLogoutOpen} className={s.popoverBtn}>
+          <svg className={s.popoverIcons} width={16} height={16}>
+            <use href={`${sprite}#icon-log-out`}></use>
+          </svg>
+          Log out
+        </button>
+      </div>
+      <Modal isOpen={settingModalOpen} onClose={handleSettingClose}>
+        <UserSettingsModal
+          isOpen={settingModalOpen}
+          onClose={handleSettingClose}
+        />
+      </Modal>
+      <Modal isOpen={logoutOpen} onClose={handleLogoutClose}>
+        <LogOutModal onClose={handleLogoutClose} />
+      </Modal>
     </div>
   );
 };
