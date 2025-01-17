aceac49cde009f9a739691a23125fdce803c519e
Daria Havlovych <daria.havlovych@gmail.com>
2025-01-17 23:10:30 +0200

    upd waterSlice+addBtn css+upd progressBar

--- src/components/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/WaterProgressBar/WaterProgressBar.jsx
@@ -36,17 +36,18 @@ const WaterProgressBar = ({ value }) => {
             },
             label: "100%",
           },
-          ...(value <= 100 && {
-            [value]: {
-              style: {
-                color: "rgba(47, 47, 47, 0.6)",
-                fontSize: "10px",
-                lineHeight: "10px",
-                fontWeight: "400",
+          ...(value <= 100 &&
+            (value <= 40 || value >= 60) && {
+              [value]: {
+                style: {
+                  color: "rgba(47, 47, 47, 0.6)",
+                  fontSize: "10px",
+                  lineHeight: "10px",
+                  fontWeight: "400",
+                },
+                label: `${value}%`,
               },
-              label: `${value}%`,
-            },
-          }),
+            }),
         }}
         railStyle={{
           backgroundColor: "#F0EFF4",
--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -3,13 +3,10 @@ import { useEffect } from "react";
 import WaterMainInfo from "../../components/WaterMainInfo/WaterMainInfo.jsx";
 import WaterDetailedInfo from "../../components/WaterDetailedInfo/WaterDetailedInfo.jsx";
 import { getUserData } from "../../redux/user/operations.js";
-import { selectError, selectLoading } from "../../redux/water/selectors.js";
 import { getWaterDay } from "../../redux/water/operations.js";
 
 const TrackerPage = () => {
   const dispatch = useDispatch();
-  // const isLoading = useSelector(selectLoading);
-  // const errorMessage = useSelector(selectError);
 
   useEffect(() => {
     const loadData = async () => {
