f11ccd21d03c6c44fdde1ca1f7464def19ace1e2
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2025-01-19 15:40:32 +0200

    fix auth err and set header

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
-import { NavLink } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { ErrorMessage, Field, Form, Formik } from "formik";
-import styles from "./signInForm.module.css";
-import { logIn } from "../../redux/user/operations.js";
-import { useDispatch } from "react-redux";
-import { orderSchemaLogin } from "../../utils/formValidation.js";
-import { selectIsRefreshing } from "../../redux/user/selectors.js"; 
-import Loader from "../Loader/Loader.jsx";
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import styles from './signInForm.module.css';
+import { logIn } from '../../redux/user/operations.js';
+import { useDispatch } from 'react-redux';
+import { orderSchemaLogin } from '../../utils/formValidation.js';
+import { selectIsRefreshing } from '../../redux/user/selectors.js';
+import Loader from '../Loader/Loader.jsx';
 const SignInForm = () => {
   const [passwordVisible, setPasswordVisible] = useState(false);
 
@@ -16,17 +16,15 @@ const SignInForm = () => {
   };
 
   const initForm = {
-    email: "",
-    password: "",
+    email: '',
+    password: '',
   };
 
   const dispatch = useDispatch();
   const isLoading = useSelector(selectIsRefreshing);
-  
-  const handleSubmit = (values, options) => {
-    dispatch(logIn(values));
 
-    options.resetForm();
+  const handleSubmit = values => {
+    dispatch(logIn(values));
   };
 
   return (
@@ -41,72 +39,73 @@ const SignInForm = () => {
         >
           <Form className={styles.form}>
             <div className={styles.formGroup}>
-              <label htmlFor="email" className={styles.label}>
+              <label htmlFor='email' className={styles.label}>
                 Email
               </label>
               <Field
-                name="email"
-                type="email"
-                id="email"
+                name='email'
+                type='email'
+                id='email'
                 className={`${styles.input} ${
                   initForm.email ? styles.error : styles.success
                 }`}
-                placeholder="Enter your email"
+                placeholder='Enter your email'
                 required
               />
               <ErrorMessage
                 className={styles.errorMessage}
-                name="email"
-                component="p"
+                name='email'
+                component='p'
               />
             </div>
             <div className={styles.formGroup}>
-              <label htmlFor="password" className={styles.label}>
+              <label htmlFor='password' className={styles.label}>
                 Password
               </label>
               <div className={styles.passwordWrapper}>
                 <Field
-                  name="password"
-                  type={passwordVisible ? "text" : "password"}
-                  id="password"
+                  name='password'
+                  type={passwordVisible ? 'text' : 'password'}
+                  id='password'
                   className={`${styles.input} ${
                     initForm.password ? styles.error : styles.success
                   }`}
-                  placeholder="Enter your password"
+                  placeholder='Enter your password'
                   required
                 />
                 <button
-                  type="button"
+                  type='button'
                   className={styles.togglePassword}
                   onClick={togglePasswordVisibility}
                 >
-                  {passwordVisible ? "🙈" : "👁️"}
+                  {passwordVisible ? '🙈' : '👁️'}
                 </button>
               </div>
               <ErrorMessage
                 className={styles.errorMessage}
-                name="password"
-                component="p"
+                name='password'
+                component='p'
               />
             </div>
-            {isLoading 
-               ? (<div className={styles.wrapperLoader}>
-                    <Loader/>
-                  </div>) 
-                : <button type="submit" className={styles.submitButton}>
-                    Sign In
-                  </button> 
-            }
+            {isLoading ? (
+              <div className={styles.wrapperLoader}>
+                <Loader />
+              </div>
+            ) : (
+              <button type='submit' className={styles.submitButton}>
+                Sign In
+              </button>
+            )}
           </Form>
         </Formik>
-       {!isLoading &&
-         <p className={styles.footerText}>
-           Don't have an account?{" "}
-           <NavLink to="/signup" className={styles.signupLink}>
-             Sign Up
-           </NavLink>
-         </p>
-        }
+        {!isLoading && (
+          <p className={styles.footerText}>
+            Don't have an account?{' '}
+            <NavLink to='/signup' className={styles.signupLink}>
+              Sign Up
+            </NavLink>
+          </p>
+        )}
       </div>
     </div>
   );
--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
-import { useSelector } from "react-redux";
-import { register } from "../../redux/user/operations.js";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { ErrorMessage, Field, Form, Formik } from "formik";
-import { NavLink } from "react-router-dom";
-import styles from "./signUpForm.module.css";
-import { orderSchemaReg } from "../../utils/formValidation.js";
-import { selectIsRefreshing } from "../../redux/user/selectors.js"; 
-import Loader from "../Loader/Loader.jsx";
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { register } from '../../redux/user/operations.js';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { NavLink } from 'react-router-dom';
+import styles from './signUpForm.module.css';
+import { orderSchemaReg } from '../../utils/formValidation.js';
+import { selectIsRefreshing } from '../../redux/user/selectors.js';
+import Loader from '../Loader/Loader.jsx';
 
 export default function SignUpForm() {
   const [passwordVisible, setPasswordVisible] = useState(false);
@@ -24,19 +24,18 @@ export default function SignUpForm() {
   };
 
   const initForm = {
-    email: "",
-    password: "",
-    confirmPassword: "",
+    email: '',
+    password: '',
+    confirmPassword: '',
   };
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const isLoading = useSelector(selectIsRefreshing)
+  const isLoading = useSelector(selectIsRefreshing);
 
-  const handleSubmit = (values, options) => {
+  const handleSubmit = values => {
     const userData = { email: values.email, password: values.password };
     dispatch(register({ credentials: userData, navigate }));
-    options.resetForm();
   };
 
   return (
@@ -49,103 +48,104 @@ export default function SignUpForm() {
           validationSchema={orderSchemaReg}
           onSubmit={handleSubmit}
         >
-          <Form action="#" className={styles.form}>
+          <Form action='#' className={styles.form}>
             <div className={styles.formGroup}>
-              <label htmlFor="email" className={styles.label}>
+              <label htmlFor='email' className={styles.label}>
                 Email
               </label>
               <Field
-                name="email"
-                type="email"
-                id="email"
+                name='email'
+                type='email'
+                id='email'
                 className={`${styles.input} ${
                   initForm.name ? styles.error : styles.success
                 }`}
-                placeholder="Enter your email"
+                placeholder='Enter your email'
                 required
               />
               <ErrorMessage
                 className={styles.errorMessage}
-                name="email"
-                component="p"
+                name='email'
+                component='p'
               />
             </div>
             <div className={styles.formGroup}>
-              <label htmlFor="RegisterPassword" className={styles.label}>
+              <label htmlFor='RegisterPassword' className={styles.label}>
                 Password
               </label>
               <div className={styles.passwordWrapper}>
                 <Field
-                  name="password"
-                  type={passwordVisible ? "text" : "password"}
-                  id="RegisterPassword"
+                  name='password'
+                  type={passwordVisible ? 'text' : 'password'}
+                  id='RegisterPassword'
                   className={`${styles.input} ${
                     initForm.password ? styles.error : styles.success
                   }`}
-                  placeholder="Enter your password"
+                  placeholder='Enter your password'
                   required
                 />
                 <button
-                  type="button"
+                  type='button'
                   className={styles.togglePassword}
                   onClick={togglePasswordVisibility}
                 >
-                  {passwordVisible ? "🙈" : "👁️"}
+                  {passwordVisible ? '🙈' : '👁️'}
                 </button>
               </div>
               <ErrorMessage
                 className={styles.errorMessage}
-                name="password"
-                component="p"
+                name='password'
+                component='p'
               />
             </div>
             <div className={styles.formGroup}>
-              <label htmlFor="repeatPassword" className={styles.label}>
+              <label htmlFor='repeatPassword' className={styles.label}>
                 Repeat password
               </label>
               <div className={styles.passwordWrapper}>
                 <Field
-                  name="confirmPassword"
-                  type={repeatPasswordVisible ? "text" : "password"}
-                  id="repeatPassword"
+                  name='confirmPassword'
+                  type={repeatPasswordVisible ? 'text' : 'password'}
+                  id='repeatPassword'
                   className={`${styles.input} ${
                     initForm.confirmPassword ? styles.error : styles.success
                   }`}
-                  placeholder="Repeat password"
+                  placeholder='Repeat password'
                   required
                 />
                 <button
-                  type="button"
+                  type='button'
                   className={styles.togglePassword}
                   onClick={toggleRepeatPasswordVisibility}
                 >
-                  {repeatPasswordVisible ? "🙈" : "👁️"}
+                  {repeatPasswordVisible ? '🙈' : '👁️'}
                 </button>
               </div>
               <ErrorMessage
                 className={styles.errorMessage}
-                name="confirmPassword"
-                component="p"
+                name='confirmPassword'
+                component='p'
               />
             </div>
-            {isLoading 
-              ? (<div className={styles.wrapperLoader}>
-                  <Loader />
-                </div>) 
-              : <button type="submit" className={styles.submitButton}>
-                    Sign In
-                </button> 
-            }
+            {isLoading ? (
+              <div className={styles.wrapperLoader}>
+                <Loader />
+              </div>
+            ) : (
+              <button type='submit' className={styles.submitButton}>
+                Sign In
+              </button>
+            )}
           </Form>
         </Formik>
-        {!isLoading &&
+        {!isLoading && (
           <p className={styles.footerText}>
-              Don't have an account?{" "}
-              <NavLink to="/signin" className={styles.signupLink}>
-                Sign In
-             </NavLink>
+            Don't have an account?{' '}
+            <NavLink to='/signin' className={styles.signupLink}>
+              Sign In
+            </NavLink>
           </p>
-        } 
+        )}
       </div>
     </div>
   );
