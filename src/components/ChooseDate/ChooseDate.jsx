b610e32e87beaf7015b27cd5e4b19ce83e6062ae
Alexey <mirar2246@gmail.com>
2025-01-19 21:36:55 +0200

    add-css-styles-dailyInfo

--- src/components/ChooseDate/ChooseDate.jsx
+++ src/components/ChooseDate/ChooseDate.jsx
@@ -1,17 +1,39 @@
-import { useSelector } from 'react-redux';
-import { selectDateDay } from '../../redux/water/selectors.js';
-import { getFormattedDate } from '../../utils/formatDate.js';
+import { useSelector } from "react-redux";
+import { selectDateDay } from "../../redux/water/selectors.js";
+import { getFormattedDate } from "../../utils/formatDate.js";
+import s from "./ChooseDate.module.css";
 
 const ChooseDate = () => {
   const getDate = useSelector(selectDateDay);
 
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const formatDate = (dateStr) => {
+    const date = new Date(dateStr);
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    return `${day}, ${month}`;
+  };
+
   const comparison = getFormattedDate(new Date());
 
-  const ChosenDay = comparison === getDate ? 'Today' : getDate;
+  const chosenDay = comparison === getDate ? "Today" : formatDate(getDate);
 
   return (
     <div>
-      <p>{ChosenDay}</p>
+      <p className={s.todayText}>{chosenDay}</p>
     </div>
   );
 };
