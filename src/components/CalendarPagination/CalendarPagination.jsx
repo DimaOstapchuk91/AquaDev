f2afa6a8927154f7c991538a21a9671ba0195e22
xd.faunredd <andrew.furevych@gmail.com>
2025-01-21 16:39:38 +0200

    add date function

--- src/components/CalendarPagination/CalendarPagination.jsx
+++ src/components/CalendarPagination/CalendarPagination.jsx
@@ -1,31 +1,14 @@
-import s from './CalendarPagination.module.css';
-import sprite from '../../assets/sprite.svg';
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import s from "./CalendarPagination.module.css";
+import { formatDateDatMonth } from "../../utils/dateFormatter";
 
 const CalendarPagination = ({
   currentDate,
   setCurrentDate,
   isPaginationDisabled,
 }) => {
-  const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
-
-  const currentMonth = months[currentDate.getMonth()];
-  const currentYear = currentDate.getFullYear();
-
-  const changeMonth = direction => {
-    setCurrentDate(prevDate => {
+  const changeMonth = (direction) => {
+    setCurrentDate((prevDate) => {
       const newDate = new Date(prevDate);
       newDate.setMonth(prevDate.getMonth() + direction);
       return newDate;
@@ -39,21 +22,19 @@ const CalendarPagination = ({
         className={s.calendarPaginationButton}
         disabled={isPaginationDisabled}
       >
-        <svg className={s.iconArrLeft} width='20' height='20'>
-          <use className={s.left} href={`${sprite}#icon-down`}></use>
-        </svg>
+        <FaChevronLeft />
       </button>
-      <p className={s.dataInfo}>{`${
-        isPaginationDisabled ? months[new Date().getMonth()] : currentMonth
-      }, ${isPaginationDisabled ? new Date().getFullYear() : currentYear}`}</p>
+      <p className={s.dataInfo}>
+        {isPaginationDisabled
+          ? formatDateDatMonth(new Date().toISOString())
+          : formatDateDatMonth(currentDate.toISOString())}
+      </p>
       <button
         onClick={() => changeMonth(1)}
         className={s.calendarPaginationButton}
         disabled={isPaginationDisabled}
       >
-        <svg className={s.iconArrRight} width='20' height='20'>
-          <use className={s.right} href={`${sprite}#icon-down`}></use>
-        </svg>
+        <FaChevronRight />
       </button>
     </div>
   );
